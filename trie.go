c978cf6469f89199e60374eb398ea85675ca9be9
Sheng Yu <yusheng.sjtu@gmail.com>
2014-01-30 20:44:42 -0800

    Support string input

--- trie.go
+++ trie.go
@@ -3,6 +3,7 @@ package trie
 
 import (
 	"bytes"
+	"strings"
 )
 
 // Value can be any type. Note that Value when added to the trie and retrieved from the trie.
@@ -10,7 +11,7 @@ type Value interface{}
 
 // Trie is an associative array where the keys are byte arrays. See http://en.wikipedia.org/wiki/Trie for details.
 type Trie struct {
-	Value    *Value
+	value    *Value
 	prefix   []byte
 	children map[byte]*Trie
 }
@@ -22,55 +23,80 @@ func NewTrie() *Trie {
 
 // Add a key value to Trie. Override the value if the same key is given again.
 func (this *Trie) Add(key []byte, value Value) {
-	this.createNode(key).Value = &value
+	this.createNode(key).value = &value
 }
 
-// Get the value added together with key. If no such key was added, return nil, false.
-func (this *Trie) Get(key []byte) (value Value, found bool) {
-	r := this.findNode(key, exactMatch)
+// Get the value associated with the key. If no such key was added, return nil, false.
+func (this *Trie) GetBytes(key []byte) (value Value, found bool) {
+	return this.get(&inputBytes{key})
+}
+
+// Same as GetBytes but works for string.
+func (this *Trie) GetString(key string) (value Value, found bool) {
+	return this.get(&inputString{key})
+}
+
+func (this *Trie) get(in input) (value Value, found bool) {
+	r := this.findNode(in, exactMatch)
 	if len(r) == 0 {
 		return Value(nil), false
 	}
-	return *r[0].trie.Value, true
+	return *r[0].trie.value, true
 }
 
 // PrefixMatch is the type of returned value of Trie's prefix matching functions.
 type PrefixMatch struct {
-	Prefix []byte
-	Value  Value
+	PrefixLength int
+	Value        Value
 }
 
 // Match the shortest prefix and associated value. If no prefix is found, return {nil, nil}, false.
-func (this *Trie) MatchShortestPrefix(input []byte) (match PrefixMatch, found bool) {
-	r := this.findNode(input, shortestPrefix)
-	if len(r) == 0 {
-		return PrefixMatch{}, false
-	}
-	return PrefixMatch{
-		Prefix: input[:r[0].prefixLength],
-		Value:  *r[0].trie.Value,
-	}, true
+func (this *Trie) MatchShortestPrefixBytes(input []byte) (match PrefixMatch, found bool) {
+	return this.matchPrefix(&inputBytes{input}, shortestPrefix)
+}
+
+// Same as MatchShortestPrefixBytes but works for string.
+func (this *Trie) MatchShortestPrefixString(input string) (match PrefixMatch, found bool) {
+	return this.matchPrefix(&inputString{input}, shortestPrefix)
 }
 
 // Match the longest prefix and associated value. If no prefix is found, return {nil, nil}, false.
-func (this *Trie) MatchLongestPrefix(input []byte) (match PrefixMatch, found bool) {
-	r := this.findNode(input, longestPrefix)
+func (this *Trie) MatchLongestPrefixBytes(input []byte) (match PrefixMatch, found bool) {
+	return this.matchPrefix(&inputBytes{input}, longestPrefix)
+}
+
+// Same as MatchLongestPrefixBytes but works for string.
+func (this *Trie) MatchLongestPrefixString(input string) (match PrefixMatch, found bool) {
+	return this.matchPrefix(&inputString{input}, longestPrefix)
+}
+
+func (this *Trie) matchPrefix(in input, mode findNodeMode) (match PrefixMatch, found bool) {
+	r := this.findNode(in, mode)
 	if len(r) == 0 {
 		return PrefixMatch{}, false
 	}
 	return PrefixMatch{
-		Prefix: input[:r[0].prefixLength],
-		Value:  *r[0].trie.Value,
+		PrefixLength: r[0].prefixLength,
+		Value:        *r[0].trie.value,
 	}, true
 }
 
 // Match all possible prefixes and associated values as a list. If no prefix is found, return an empty list.
-func (this *Trie) MatchAllPrefixes(input []byte) []PrefixMatch {
-	r := this.findNode(input, allPrefixex)
+func (this *Trie) MatchAllPrefixesBytes(in []byte) []PrefixMatch {
+	return this.matchAllPrefixes(&inputBytes{in})
+}
+
+// Same as MatchAllPrefixesBytes but works for string input.
+func (this *Trie) MatchAllPrefixesString(in string) []PrefixMatch {
+	return this.matchAllPrefixes(&inputString{in})
+}
+
+func (this *Trie) matchAllPrefixes(in input) []PrefixMatch {
+	r := this.findNode(in, allPrefixex)
 	result := make([]PrefixMatch, len(r))
 	for i, v := range r {
-		result[i].Prefix = input[:v.prefixLength]
-		result[i].Value = *v.trie.Value
+		result[i].PrefixLength = v.prefixLength
+		result[i].Value = *v.trie.value
 	}
 	return result
 }
@@ -120,30 +146,77 @@ type findNodeResult struct {
 	trie         *Trie
 }
 
-func (this *Trie) findNode(key []byte, mode findNodeMode) []*findNodeResult {
+type input interface {
+	end() bool
+	char() byte
+	hasPrefix([]byte) bool
+	advance(int)
+}
+
+type inputBytes struct {
+	b []byte
+}
+
+func (i *inputBytes) end() bool {
+	return len(i.b) == 0
+}
+
+func (i *inputBytes) char() byte {
+	return i.b[0]
+}
+
+func (i *inputBytes) hasPrefix(prefix []byte) bool {
+	return bytes.HasPrefix(i.b, prefix)
+}
+
+func (i *inputBytes) advance(n int) {
+	i.b = i.b[n:]
+}
+
+type inputString struct {
+	s string
+}
+
+func (i *inputString) end() bool {
+	return len(i.s) == 0
+}
+
+func (i *inputString) char() byte {
+	return i.s[0]
+}
+
+func (i *inputString) hasPrefix(prefix []byte) bool {
+	return strings.HasPrefix(i.s, string(prefix))
+}
+
+func (i *inputString) advance(n int) {
+	i.s = i.s[n:]
+}
+
+func (this *Trie) findNode(key input, mode findNodeMode) []*findNodeResult {
 	result := []*findNodeResult{}
 	length := 0
-	for len(key) != 0 {
-		if this.Value != nil && (mode == shortestPrefix || mode == allPrefixex) {
+	for !key.end() {
+		if this.value != nil && (mode == shortestPrefix || mode == allPrefixex) {
 			result = append(result, &findNodeResult{length, this})
 			if mode == shortestPrefix {
 				return result
 			}
 		}
-		firstByte := key[0]
+		firstByte := key.char()
 		child, has := this.children[firstByte]
-		has = has && bytes.HasPrefix(key, child.prefix)
+		has = has && key.hasPrefix(child.prefix)
 		if !has {
-			if this.Value != nil && mode == longestPrefix {
+			if this.value != nil && mode == longestPrefix {
 				result = append(result, &findNodeResult{length, this})
 			}
 			return result
 		}
-		key = key[len(child.prefix):]
+		key.advance(len(child.prefix))
 		length += len(child.prefix)
 		this = child
 	}
-	if this.Value != nil {
+	if this.value != nil {
 		result = append(result, &findNodeResult{length, this})
 	}
 	return result
--- trie_test.go
+++ trie_test.go
@@ -1,7 +1,6 @@
 package trie
 
 import (
-	"bytes"
 	"testing"
 )
 
@@ -37,10 +36,10 @@ func createTestTrie() *Trie {
 	return t
 }
 
-func TestTrieGet(t *testing.T) {
+func TestTrieGetBytes(t *testing.T) {
 	trie := createTestTrie()
 	for _, k := range keys {
-		v, ok := trie.Get([]byte(k))
+		v, ok := trie.GetBytes([]byte(k))
 		if ok != true {
 			t.Errorf("Unable to find key %s", k)
 		}
@@ -49,7 +48,7 @@ func TestTrieGet(t *testing.T) {
 		}
 	}
 	for _, k := range nonKeys {
-		v, ok := trie.Get([]byte(k))
+		v, ok := trie.GetBytes([]byte(k))
 		if ok == true {
 			t.Errorf("Unexpected key %s, value %v", k, v)
 		}
@@ -59,15 +58,38 @@ func TestTrieGet(t *testing.T) {
 	}
 }
 
-func TestTrieMatchAllPrefixes(t *testing.T) {
+func TestTrieGetString(t *testing.T) {
 	trie := createTestTrie()
-	r := trie.MatchAllPrefixes([]byte(content))
+	for _, k := range keys {
+		v, ok := trie.GetString(k)
+		if ok != true {
+			t.Errorf("Unable to find key %s", k)
+		}
+		if v.(string) != k {
+			t.Errorf("Wrong value %v, expected %v", v, k)
+		}
+	}
+	for _, k := range nonKeys {
+		v, ok := trie.GetString(k)
+		if ok == true {
+			t.Errorf("Unexpected key %s, value %v", k, v)
+		}
+		if v != nil {
+			t.Errorf("Value should be nil if not found, but %v", v)
+		}
+	}
+}
+
+func TestTrieMatchAllPrefixesBytes(t *testing.T) {
+	trie := createTestTrie()
+	r := trie.MatchAllPrefixesBytes([]byte(content))
 	if len(r) != len(prefixes) {
 		t.Fatalf("Wrong length of prefixes %v vs. %v)", r, prefixes)
 	}
 	for i, p := range prefixes {
-		if !bytes.Equal(r[i].Prefix, []byte(p)) {
-			t.Errorf("Wrong prefix[%d] %s vs. %s", i, r[i].Prefix, p)
+		prefix := content[:r[i].PrefixLength]
+		if prefix != p {
+			t.Errorf("Wrong prefix[%d] %s vs. %s", i, prefix, p)
 		}
 		if r[i].Value.(string) != p {
 			t.Errorf("Wrong prefix[%d] %s vs. %s", i, r[i].Value.(string), p)
@@ -75,47 +97,114 @@ func TestTrieMatchAllPrefixes(t *testing.T) {
 	}
 }
 
-func TestTrieMatchShortestPrefix(t *testing.T) {
+func TestTrieMatchAllPrefixesString(t *testing.T) {
 	trie := createTestTrie()
-	v, ok := trie.MatchShortestPrefix([]byte(content))
+	r := trie.MatchAllPrefixesString(content)
+	if len(r) != len(prefixes) {
+		t.Fatalf("Wrong length of prefixes %v vs. %v)", r, prefixes)
+	}
+	for i, p := range prefixes {
+		prefix := content[:r[i].PrefixLength]
+		if prefix != p {
+			t.Errorf("Wrong prefix[%d] %s vs. %s", i, prefix, p)
+		}
+		if r[i].Value.(string) != p {
+			t.Errorf("Wrong prefix[%d] %s vs. %s", i, r[i].Value.(string), p)
+		}
+	}
+}
+
+func TestTrieMatchShortestPrefixBytes(t *testing.T) {
+	trie := createTestTrie()
+	v, ok := trie.MatchShortestPrefixBytes([]byte(content))
 	expected := prefixes[0]
 	if !ok {
 		t.Errorf("Should find shortest prefix")
 	}
-	if !bytes.Equal(v.Prefix, []byte(expected)) {
-		t.Errorf("Wrong prefix %s vs. %s", v.Prefix, expected)
+	prefix := content[:v.PrefixLength]
+	if prefix != expected {
+		t.Errorf("Wrong prefix %s vs. %s", prefix, expected)
 	}
 	if v.Value.(string) != expected {
 		t.Errorf("Wrong prefix %s vs. %s", v.Value.(string), expected)
 	}
 }
 
-func TestTrieMatchLongestPrefix(t *testing.T) {
+func TestTrieMatchShortestPrefixString(t *testing.T) {
 	trie := createTestTrie()
-	v, ok := trie.MatchLongestPrefix([]byte(content))
+	v, ok := trie.MatchShortestPrefixString(content)
+	expected := prefixes[0]
+	if !ok {
+		t.Errorf("Should find shortest prefix")
+	}
+	prefix := content[:v.PrefixLength]
+	if prefix != expected {
+		t.Errorf("Wrong prefix %s vs. %s", prefix, expected)
+	}
+	if v.Value.(string) != expected {
+		t.Errorf("Wrong prefix %s vs. %s", v.Value.(string), expected)
+	}
+}
+
+func TestTrieMatchLongestPrefixBytes(t *testing.T) {
+	trie := createTestTrie()
+	v, ok := trie.MatchLongestPrefixBytes([]byte(content))
 	expected := prefixes[len(prefixes)-1]
 	if !ok {
 		t.Errorf("Should find longest prefix")
 	}
-	if !bytes.Equal(v.Prefix, []byte(expected)) {
-		t.Errorf("Wrong prefix %s vs. %s", v.Prefix, expected)
+	prefix := content[:v.PrefixLength]
+	if prefix != expected {
+		t.Errorf("Wrong prefix %s vs. %s", prefix, expected)
 	}
 	if v.Value.(string) != expected {
 		t.Errorf("Wrong prefix %s vs. %s", v.Value.(string), expected)
 	}
 }
 
-func TestNoPrefixContent(t *testing.T) {
+func TestTrieMatchLongestPrefixString(t *testing.T) {
 	trie := createTestTrie()
-	r := trie.MatchAllPrefixes([]byte(noPrefixContent))
+	v, ok := trie.MatchLongestPrefixString(content)
+	expected := prefixes[len(prefixes)-1]
+	if !ok {
+		t.Errorf("Should find longest prefix")
+	}
+	prefix := content[:v.PrefixLength]
+	if prefix != expected {
+		t.Errorf("Wrong prefix %s vs. %s", prefix, expected)
+	}
+	if v.Value.(string) != expected {
+		t.Errorf("Wrong prefix %s vs. %s", v.Value.(string), expected)
+	}
+}
+
+func TestNoPrefixContentBytes(t *testing.T) {
+	trie := createTestTrie()
+	r := trie.MatchAllPrefixesBytes([]byte(noPrefixContent))
 	if len(r) != 0 {
 		t.Errorf("Unexpected result: %v", r)
 	}
-	v, ok := trie.MatchShortestPrefix([]byte(noPrefixContent))
+	v, ok := trie.MatchShortestPrefixBytes([]byte(noPrefixContent))
 	if ok {
 		t.Errorf("Unexpected shortest prefix found %v", v)
 	}
-	v, ok = trie.MatchLongestPrefix([]byte(noPrefixContent))
+	v, ok = trie.MatchLongestPrefixBytes([]byte(noPrefixContent))
+	if ok {
+		t.Errorf("Unexpected longest prefix found %v", v)
+	}
+}
+
+func TestNoPrefixContentString(t *testing.T) {
+	trie := createTestTrie()
+	r := trie.MatchAllPrefixesString(noPrefixContent)
+	if len(r) != 0 {
+		t.Errorf("Unexpected result: %v", r)
+	}
+	v, ok := trie.MatchShortestPrefixString(noPrefixContent)
+	if ok {
+		t.Errorf("Unexpected shortest prefix found %v", v)
+	}
+	v, ok = trie.MatchLongestPrefixString(noPrefixContent)
 	if ok {
 		t.Errorf("Unexpected longest prefix found %v", v)
 	}
